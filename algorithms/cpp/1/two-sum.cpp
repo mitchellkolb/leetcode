05bbd3a876bb5545bbe238dcf207a7408360d7e1
Mitchell-kolb <mkolb321@gmail.com>
2024-10-04 04:02:49 -0700

    Added gitignore for all future .exes for cpp files

--- algorithms/cpp/1/two-sum.cpp
+++ algorithms/cpp/1/two-sum.cpp
@@ -1,18 +1,10 @@
 
 
-#include <stdio.h> // Include the standard input/output library for printf
 
 
-class Solution {
-public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        
-    }
-};
-
+#include <iostream>
 
 int main() {
-    // Print "Hello, World!" to the console
-    printf("Hello, World!\n");
-    return 0; // Return 0 to indicate successful completion
+    std::cout << "\n\nHello, World!\n\n" << std::endl;
+    return 0;
 }
