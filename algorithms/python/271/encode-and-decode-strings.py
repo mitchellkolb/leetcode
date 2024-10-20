a8ae4db7672c7c095ea8917abe2713a006be4fd7
Mitchell-kolb <mkolb321@gmail.com>
2024-10-20 02:33:52 -0700

    Remade 271 to be better

--- algorithms/python/271/encode-and-decode-strings.py
+++ algorithms/python/271/encode-and-decode-strings.py
@@ -25,7 +25,6 @@ Constraints:
     strs[i] contains only UTF-8 characters.
 
 """
-from functools import reduce
 
 class Solution:
 
@@ -46,8 +45,27 @@ class Solution:
 
     def decode(self, s: str) -> list[str]:
         
-        indice = 0
-        seperatedStr = []
+        result = []
+        left, right = 0, 0
+
+        while left < len(s):
+
+            #Getting the size before the @
+            while s[right] != "@":
+                right += 1
+            
+            #Add new string to list
+            size = int(s[left:right])
+            result.append(s[right + 1:right + 1 + size])
+
+            #Adjust left/right for next iteration
+            left = right + size + 1
+            right = left + 1
+
+
+        return result
+
+
 
         
 
@@ -58,11 +76,65 @@ repeat on the remaining rest of string with the first step
 """
 
 solution = Solution()
-tests = [["a", "bc", "d"], ["we","say",":","yes"], ["abc", "", "def"], ["ert", "sdf", ""], ["", "fdg", "cvb"], ["oneWord"], [""], []]
+tests = [["we","say",":","yes","!@#$%^&*()"], ["a", "bc", "d"], ["we","say",":","yes"], ["abc", "", "def"], ["ert", "sdf", ""], ["", "fdg", "cvb"], ["oneWord"], [""], []]
 
 for test in tests:
     print(f"\n", test)
     resultInput = (solution.encode(test))
     print(resultInput)
-    #print(solution.decode(resultInput))
+    print(solution.decode(resultInput))
 
+
+
+"""
+# This is an old crazy version I somehow got to pass all the cases but it is rough to read through
+
+class Solution:
+
+    def encode(self, strs: list[str]) -> str:
+        newCode = ""
+
+        for word in strs:
+            lengthWord = str(len(word))
+            delimiter = "@"
+            encodedWord = lengthWord + delimiter + word
+            
+            newCode = newCode + encodedWord
+            
+        return newCode
+        
+#Format of encode: (size of string)(delimiter)(string)
+
+
+    def decode(self, s: str) -> list[str]:
+        
+        result = []
+
+        while s:
+
+            #Assume that the string always starts at the start that I need it to
+            lengthWord = ""
+            stopper = True
+            while stopper == True:
+                if s[0] != "@":
+                    lengthWord = lengthWord + s[0]
+                    s = s[1:]
+                else:
+                    stopper = False
+                
+            lengthWord = int(lengthWord)
+            
+            delimiter = s[:1]
+            s = s[1:]
+            newStr = ""
+
+            if lengthWord <= len(s):
+                newStr = s[:lengthWord]
+                s = s[lengthWord:]
+                result.append(newStr)
+            else:
+                print("Error in while has occured")
+
+        return result
+
+"""
