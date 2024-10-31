68810bf408165bac7f3f0ebe78bf94b36f837376
Mitchell-kolb <mkolb321@gmail.com>
2024-10-30 17:59:01 -0700

    New Chapter Algos added

---
+++ data-structures/3-fibonaci.py
@@ -0,0 +1,19 @@
+
+
+
+def fibonaci(target: int) -> int:
+    nums = 2
+
+    return nums
+
+
+"""
+Selectio
+
+It go
+"""
+
+
+nums = [58, 43]
+target = 10
+print(fibonaci(nums, target))
---
+++ data-structures/3-stack.py
@@ -0,0 +1,19 @@
+
+
+
+def stack(target: int) -> int:
+    nums = 2
+
+    return nums
+
+
+"""
+Selectio
+
+It go
+"""
+
+
+nums = [58, 43]
+target = 10
+print(stack(nums, target))
