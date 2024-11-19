a921ab5768f3eb5dea4e13eadfc7596c1c793bde
Mitchell-kolb <mkolb321@gmail.com>
2024-11-18 21:11:15 -0800

    Data Structure Added from book: BFS

---
+++ data-structures/bfs.py
@@ -0,0 +1,3 @@
+
+
+# BFS or breath first search is like exploring a map layer by layer. Starting from a node, it checks all its immediate neighbors first before diving deeper into their neighbors. Think of it like a ripple in water, spreading outward evenly, ensuring you cover everything close before heading further away.
