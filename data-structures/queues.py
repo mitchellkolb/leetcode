7891af67f3ac674b66fb439525ec59577802ac50
Mitchell-kolb <mkolb321@gmail.com>
2024-11-18 20:58:06 -0800

    Data Structure Added from book: Queues

---
+++ data-structures/queues.py
@@ -0,0 +1,3 @@
+
+
+# A queue is a First in First out data structure that uses enqueue and dequeue as a way to push and pop items from the list. In python you can import queues from the collections library or just use a list and pop from the front of it and let the default append push items to the back of the list. In C you would use a linked list with structs pointing to each other
