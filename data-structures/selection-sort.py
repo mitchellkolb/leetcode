ff5ebcaca091f0e28e9f73e5e2e3e48dc785a8ec
Mitchell-kolb <mkolb321@gmail.com>
2024-11-19 20:36:08 -0800

    Data Structures Time Complexity: Selection Sort

--- data-structures/selection-sort.py
+++ data-structures/selection-sort.py
@@ -23,7 +23,9 @@ Selection sort is a N^2 time complexity algo
 
 It goes by you having a left check and a right check and you shift the right check all the way through the list until the end and on every iteration you compare the left and right value for whos is the minimum. If left < right move onto the next right item in the list is right < left then you switch the values of right and left so left now has the global minima after you cycle right through the entire list. After right reaches the end you adjust left by one and repeat the cycle again until you find the 2nd place minima and then by the time left is the 2nd to last item in the list the list is sorted becuase the least value is to the left of the current left value.
 """
-
+"""
+The Time Complexity of Selection Sort is O(n^2) where n is the size of the array
+"""
 
 #       0    1  2    3   4   5   6    7   8   9  10  11
 nums = [58, 43, 66, 85, 100, 61, 87, 17, 89, 66, 96, 23]
