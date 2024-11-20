3e986786a8227b0a29ecc80bda4a04c8cd964457
Mitchell-kolb <mkolb321@gmail.com>
2024-11-19 20:37:22 -0800

    Data Structures Time Complexity: Stack

--- data-structures/stack.py
+++ data-structures/stack.py
@@ -18,6 +18,10 @@ class Stack:
 Stacks are a way of storing and handling data. While it is used in leetcode for sorting and searching problems its purpose is a way to store items in a first in last out format.
 """
 
+"""
+The Time Complexity of using the stack data structure in python is esentially just using python lists which is O(1) for push, pop, peek
+"""
+
 stack = Stack()
 nums = [1,2,3,4,5]
 for num in nums:
