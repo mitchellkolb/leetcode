21528d813814ed695a9f1822275d394b4697eb2c
Mitchell-kolb <mkolb321@gmail.com>
2024-11-19 20:32:05 -0800

    Data Structures Time Complexity: Queues

--- data-structures/queues.py
+++ data-structures/queues.py
@@ -2,6 +2,10 @@
 
 # A queue is a First in First out data structure that uses enqueue and dequeue as a way to push and pop items from the list. In python you can import queues from the collections library or just use a list and pop from the front of it and let the default append push items to the back of the list. In C you would use a linked list with structs pointing to each other
 
+"""
+The Time Complexity of using queues that use python lists and/or collections are
+Enqueue O(1), Dequeue O(1), Peeking O(1)
+"""
 
 
 class QueueList:
